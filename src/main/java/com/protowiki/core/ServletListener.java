1c7a7b2c7b860e8cba5dcac38256e1063b0c7fee
nvurgaft <slipperygate@gmail.com>
2016-01-06 21:39:25 +0000

    initial commit

---
+++ src/main/java/com/protowiki/core/ServletListener.java
@@ -0,0 +1,27 @@
+
+package com.protowiki.core;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Kobi
+ */
+public class ServletListener implements ServletContextListener {
+
+    private static Logger logger = LoggerFactory.getLogger(ServletListener.class);
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("Servlet context initialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("Servlet context destroyed");
+    }
+    
+}
